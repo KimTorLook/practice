f07beacb146227fb34c040d999f8c5b546ceeb55
LAPTOP-8A6EL2LQ\lokwa <lokwahsmallho@msn.com>
2024-06-07 18:08:26 +0800

    no message

--- schedule/main.py
+++ schedule/main.py
@@ -15,13 +15,18 @@ schedule
 '''
 
 
-from matching import perm
-from scheduel import timeschudule
-from random import 
+from random import randrange
 position = ['CEO', 'CFO', 'CTO', 'HR Manager', 'Sale Manager', 'Maketing Manager', 'Purchasing Manager', 'Manufactoring Manager', 'General Manager', 'Customer Service Manager']
 
-# 1. 先random 1個 sequence 抽每日的順序
-# 2. permutation 個 persons , 並設有sequence編號
-# 3. 將permutation sequence 與 random sequence match up 
-# 4. 將每個sequence 最後 append 多一個 person 作為最後一個position
+# 1. 先random 1個 sequence 抽每日的順序                                         ok
+# 2. permutation 個 persons , 並設有sequence編號                                ok
+# 3. 將每個sequence 最後 append 多一個 person 作為最後一個position
+# 4. 將permutation sequence 與 random sequence match up 
 # 5. 用 for loop 將matching 完整呈現
+
+def daily_random_sequence():
+    for i in range(1,367):
+        a = randrange(1,366,1)
+        print(a)
+        
+daily_random_sequence()
--- schedule/matching.py
+++ schedule/matching.py
@@ -1,13 +1,29 @@
 from itertools import permutations 
+from random import choice
 
 persons = ['Jay', 'JC', 'Henry', 'Ian', 'Bi', 'Kelly', 'David', 'Emma', 'Fany']
 match = []
 
- 
-perm = permutations(persons, 9) 
- 
-for i in perm: 
-    print(i)
-    match.append(i)
+def permutat():
+    perm = permutations(persons, 9) 
+    perm_in_list = list(perm)
+    tenth_person = []   #簡單啲，將兩組序列分別儲存，然後mix up
+    
 
 
+    counter = 0
+
+    for iterate in perm: 
+
+
+
+
+
+
+        counter +=1    
+        permtat_outcome = f"{counter}. {iterate} "
+        match.append(permtat_outcome)
+        print(match)
+
+
+permutat()
